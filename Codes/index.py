a980774508333b8bd7e3945d23478905b53410f7
Yashsaxenago <yashsaxenaald@gmail.com>
2024-09-03 18:31:50 +0530

    second

--- Codes/index.py
+++ Codes/index.py
@@ -1,5 +1,30 @@
 print("hello yash \n this side yash")
 
-print("hello yash \n this side yash")
-print("hello yash \n this side yash")
-print("hello yash \n this side yash")
+# for putting values in double quotes
+print("hello yash \n this side yash\"Mr gogo master\" he is as good as panda")
+
+# for putting seprator between output
+
+print("yash",89,"age=23",sep="~")
+
+a = complex(8, 2)
+b = True
+c = "Harry"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
