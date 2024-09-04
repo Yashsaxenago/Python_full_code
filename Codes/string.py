9a5d33bfae587944e877a835c9080f41c051903d
Yashsaxenago <yashsaxenaald@gmail.com>
2024-09-04 13:35:10 +0530

    third

---
+++ Codes/string.py
@@ -0,0 +1,15 @@
+name="this is yash\n"
+age ='he is 23 right "now", but in the next year he will be 24\n'
+relationship ='''he is single from last one year\n'''
+
+print(name)
+print(age)
+print(relationship)
+
+for character in name:
+    print(character)
+
+
+for letter in age:
+    print(letter)
+
