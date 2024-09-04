6f83c79856e13fba354dab4fb10866f3e976b90b
Yashsaxenago <yashsaxenaald@gmail.com>
2024-09-04 12:58:17 +0530

    third day

---
+++ Codes/input.py
@@ -0,0 +1,8 @@
+a= input("enter your name :")
+print(a)
+
+x=input("enter the first number")
+y=input("enter the second number")
+
+print(int(x)+int(y))
+print(x+y) # because this is taking as a string value for this we have to use type conversion.
