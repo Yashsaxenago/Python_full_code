535f466be931ab2e71219acf394686691bd79b13
Yashsaxenago <yashsaxenaald@gmail.com>
2024-09-11 12:29:06 +0530

    seven day

---
+++ Codes/loop.py
@@ -0,0 +1,23 @@
+# for loop
+x="yash is good boy"
+for i in x:
+    print(i)
+
+
+color=["red","yellow","green"]
+
+for j in color:
+    print(j)
+    for k in j:
+        print(k)
+
+for l in range(10):
+    print(l)
+
+for m in range (300,500):
+    print(m)
+
+for n in range(1,9,2):
+ print(n)
+
+ #while loop
