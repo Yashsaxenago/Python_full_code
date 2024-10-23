571625dc25705147cc09205d2f289841fe2a930e
Yashsaxenago <yashsaxenaald@gmail.com>
2024-10-24 00:39:29 +0530

    10 day

---
+++ Codes/list.py
@@ -0,0 +1,16 @@
+marks =[1,2,3]
+print(type(marks))
+print(marks)
+print(marks[0])
+print(marks[1])
+print(marks[2])
+
+yash =["yash",345,True]
+print(yash[2])
+print(marks[-1])
+
+m=[1,2,3,4,5,6,7]
+#to copy this list :
+
+l=m.copy()
+print(l)
---
+++ Codes/tuples.py
@@ -0,0 +1,11 @@
+# we can't change the tuples
+
+tup =(1,2,"yash")
+print(type(tup),tup)
+
+# for single element we use , example
+tupp=(1,)
+print(type(tupp),tupp) 
+
+print(tup[1])
+print(tup[-2])
