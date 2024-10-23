2cecdea18cc2ec07c1c4f6a18a3b15f3042ed8ed
Yashsaxenago <yashsaxenaald@gmail.com>
2024-10-23 21:51:51 +0530

    9

---
+++ Codes/function.py
@@ -0,0 +1,16 @@
+def calculatemean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+a=10
+b=20
+calculatemean(a,b)
+
+def SumOfTwoNum(a,b):
+    pass 
+# pass means mai function ki body baad mai likhoga you can go ahead and write the code.
+
+c=10
+d=34
+
+SumOfTwoNum(c,d)
